b8b9a09243821b35b49b620fe392732d3cd11acf
Edwin Lima <edwin.limaa@gmail.com>
2019-11-21 21:55:22 -0300

    usando ngIf ngFor.

--- src/app/ng-if-ng-for/ng-if-ng-for.component.ts
+++ src/app/ng-if-ng-for/ng-if-ng-for.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class NgIfNgForComponent implements OnInit {
 
   numero: number;
+  arrayNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  arrayNomes = ["Fiat","Wolksvagem","Renaut","Hyundai","Honda", "Ford", "Chevrolet"];
   constructor() { }
 
   ngOnInit() {
